c5310c659c0ea88d3c37619d31c7f7f2ae3e9b7f
Khai <muankhai10@gmail.com>
2024-04-20 21:19:57 -0400

    fetching user data

--- client/src/Account.jsx
+++ client/src/Account.jsx
@@ -1,21 +1,20 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const API = "https://fsa-capstone.onrender.com/api";
+const API = "https://fsa-capstone.onrender.com/api/auth";
 
 const Account = ({ token }) => {
   const [errorMessage, setErrorMessage] = useState('');
-  const [auth, setAuth] = useState(null); // Adding auth state
   const history = useNavigate();
 
   useEffect(() => {
     authenticate();
-  }, [token]);
+  }, [token]); 
 
   const authenticate = async () => {
     if (token) {
       try {
-        const response = await fetch(`${API}/users/`, {
+        const response = await fetch(`${API}/me`, {
           method: 'GET',
           headers: {
             Authorization: token
@@ -24,8 +23,6 @@ const Account = ({ token }) => {
         if (!response.ok) {
           throw new Error('Failed to fetch user data');
         }
-        const userData = await response.json(); // Parse user data from response
-        setAuth(userData); // Set the user data to the auth state
       } catch (error) {
         console.error('Error fetching user data:', error);
         setErrorMessage('Failed to fetch user data. Please try again later.');
@@ -35,9 +32,8 @@ const Account = ({ token }) => {
 
   const logout = () => {
     window.localStorage.removeItem('token');
-    history('/products');
+    window.location.reload(history('/products'));
   };
-
   return (
     <div className="account-container">
       <div className="navigation-item">
@@ -47,15 +43,6 @@ const Account = ({ token }) => {
       </div>
       <h2 className="account-heading">Account Information</h2>
       {errorMessage && <p className="error">{errorMessage}</p>}
-      {auth && ( // Render user details if auth state is not null
-        <div className="user-details">
-          <p><strong>First Name:</strong> {auth.first_name}</p>
-          <p><strong>Last Name:</strong> {auth.last_name}</p>
-          <p><strong>Email:</strong> {auth.email}</p>
-          <p><strong>Address:</strong> {auth.address}</p>
-          <p><strong>Payment Method:</strong> {auth.payment_method}</p>
-        </div>
-      )}
     </div>
   );
 };
